d01be8093786e59660c11ba02c77012a9f23d001
Zyrave <rob.rivermark@gmail.com>
2018-11-26 17:17:19 +0700

    Add registerfeature

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,8 @@
+import * as bcrypt from 'bcryptjs';
+
 import { ResolverMap } from './types/graphql-utils';
 import { GQL } from './types/schema';
+import { User } from './entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -7,8 +10,17 @@ export const resolvers: ResolverMap = {
       `Hello ${name || 'World'}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
-      return email + password;
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+      await user.save();
+      return true;
     }
   }
 };
