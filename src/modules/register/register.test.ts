5cc22c610bc5d9bc904b2a0bc14ad50a0e257427
Zyrave <rob.rivermark@gmail.com>
2018-11-27 17:11:49 +0700

    Validate input

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -3,6 +3,12 @@ import { getConnection } from 'typeorm';
 
 import { User } from '../../entity/User';
 import { startServer } from '../../../src/startServer';
+import {
+  emailNotLongEnough,
+  duplicateEmail,
+  invalidEmail,
+  passwordNotLongEnough
+} from './errorMessages';
 
 interface AddressInfo {
   address: string;
@@ -18,12 +24,12 @@ beforeAll(async () => {
   getHost = () => `http://127.0.0.1:${port}`;
 });
 
-const email = 'tom@tom.com';
-const password = 'jaasdfad';
+const email = 'tom@bob.com';
+const password = 'jalksdf';
 
-const mutation = `
+const mutation = (e: string, p: string) => `
   mutation {
-    register(email: "${email}", password: "${password}") {
+    register(email: "${e}", password: "${p}") {
       path
       message
     }
@@ -31,7 +37,8 @@ const mutation = `
 `;
 
 test('Register user', async () => {
-  const response = await request(getHost(), mutation);
+  // Make sure we can register a user
+  const response = await request(getHost(), mutation(email, password));
   expect(response).toEqual({ register: null });
 
   const users = await User.find({ where: { email } });
@@ -41,9 +48,58 @@ test('Register user', async () => {
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
 
-  const response2: any = await request(getHost(), mutation);
+  // Test for duplicate emails
+  const response2: any = await request(getHost(), mutation(email, password));
   expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual('email');
+  expect(response2.register[0]).toEqual({
+    path: 'email',
+    message: duplicateEmail
+  });
+
+  // Catch bad email
+  const response3: any = await request(getHost(), mutation('b', password));
+  expect(response3).toEqual({
+    register: [
+      {
+        path: 'email',
+        message: emailNotLongEnough
+      },
+      {
+        path: 'email',
+        message: invalidEmail
+      }
+    ]
+  });
+
+  // Catch bad password
+  const response4: any = await request(getHost(), mutation(email, 'ad'));
+  expect(response4).toEqual({
+    register: [
+      {
+        path: 'password',
+        message: passwordNotLongEnough
+      }
+    ]
+  });
+
+  // Catch bad password and bad email
+  const response5: any = await request(getHost(), mutation('df', 'ad'));
+  expect(response5).toEqual({
+    register: [
+      {
+        path: 'email',
+        message: emailNotLongEnough
+      },
+      {
+        path: 'email',
+        message: invalidEmail
+      },
+      {
+        path: 'password',
+        message: passwordNotLongEnough
+      }
+    ]
+  });
 });
 
 afterAll(async () => {
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,18 +1,43 @@
 import * as bcrypt from 'bcryptjs';
+import * as yup from 'yup';
 
 import { ResolverMap } from '../../types/graphql-utils';
 import { GQL } from '../../types/schema';
 import { User } from '../../entity/User';
+import { formatYupError } from './../../utils/formatYupError';
+import {
+  emailNotLongEnough,
+  duplicateEmail,
+  invalidEmail,
+  passwordNotLongEnough
+} from './errorMessages';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+});
 
 export const resolvers: ResolverMap = {
   Query: {
     bye: () => 'bye'
   },
   Mutation: {
-    register: async (
-      _,
-      { email, password }: GQL.IRegisterOnMutationArguments
-    ) => {
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (err) {
+        return formatYupError(err);
+      }
+
+      const { email, password } = args;
+
       const userAlreadyExists = await User.findOne({
         where: { email },
         select: ['id']
@@ -22,7 +47,7 @@ export const resolvers: ResolverMap = {
         return [
           {
             path: 'email',
-            message: 'already taken'
+            message: duplicateEmail
           }
         ];
       }
