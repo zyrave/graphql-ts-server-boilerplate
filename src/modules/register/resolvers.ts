04262624097492fc5772454160b88c1faf3dc016
Zyrave <rob.rivermark@gmail.com>
2018-11-27 16:21:41 +0700

    Handle error in GraphQL

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -13,13 +13,29 @@ export const resolvers: ResolverMap = {
       _,
       { email, password }: GQL.IRegisterOnMutationArguments
     ) => {
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ['id']
+      });
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: 'email',
+            message: 'already taken'
+          }
+        ];
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
         password: hashedPassword
       });
+
       await user.save();
-      return true;
+
+      return null;
     }
   }
 };
